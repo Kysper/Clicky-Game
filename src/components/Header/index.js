dfc3aa762880ed0f9d796f12b91955772f95fc43
Kysper <kysper50@gmail.com>
2019-01-10 12:55:05 -0600

    Got bootstrap to work by adding the CDN to the index.html in the public folder

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,9 +8,9 @@ class Header extends React.Component {
     return (
       <div id='wrapper'>
           <div className="d-flex bd-highlight mb-3">
-            <div className="mr-auto p-2 bd-highlight">SB Memory Click</div>
-            <div className="p-2 bd-highlight">Click on a card to start!</div>
-            <div className="p-2 bd-highlight">Score:{this.props.score}</div>
+            <div className="mr-auto p-2 bd-highlight"><h3>Super Smash Memory Game</h3></div>
+            <div className="p-2 bd-highlight"><h4>Click on a card to start!</h4></div>
+            <div className="p-2 bd-highlight"><h4>Score:{this.props.score}</h4></div>
           </div>
           <div className="text-center">        
           <img id='logo' src={logo} alt='logo'/>
