7f968c63952e9407a8a4a98197a905eb39083b2d
Kysper <kysper50@gmail.com>
2019-01-10 11:01:48 -0600

    Updates

---
+++ precache-manifest.7246079adb4e7469e25f6533ae2ffacd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7e151ee41be1e534a75ebff907d7874",
+    "url": "/Clicky-Game/static/media/littlemac.c7e151ee.png"
+  },
+  {
+    "revision": "981ed03bb81a96a60390",
+    "url": "/Clicky-Game/static/css/main.5fa1e016.chunk.css"
+  },
+  {
+    "revision": "4ad756683673bcb3be35",
+    "url": "/Clicky-Game/static/js/1.4ad75668.chunk.js"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "76cae91e1fd8524704e5ece2479c40f2",
+    "url": "/Clicky-Game/static/media/ssbu.76cae91e.jpg"
+  },
+  {
+    "revision": "10d25c86d5b9e6d50120c5d519addd09",
+    "url": "/Clicky-Game/static/media/captainfalcon.10d25c86.png"
+  },
+  {
+    "revision": "9a647b72c1c2f9425fc59dd69c3a4a11",
+    "url": "/Clicky-Game/static/media/diddykong.9a647b72.png"
+  },
+  {
+    "revision": "6d99122704bdc3c666c1f7250cd88a37",
+    "url": "/Clicky-Game/static/media/duckhunt.6d991227.png"
+  },
+  {
+    "revision": "5eb02e706bb013ba012d4e2939f6ed03",
+    "url": "/Clicky-Game/static/media/fox.5eb02e70.png"
+  },
+  {
+    "revision": "31f9d4ab696ea7c64b75444398dff708",
+    "url": "/Clicky-Game/static/media/kirby.31f9d4ab.png"
+  },
+  {
+    "revision": "b50bb2721778650a2d5d13be8c7b9cff",
+    "url": "/Clicky-Game/static/media/link.b50bb272.png"
+  },
+  {
+    "revision": "981ed03bb81a96a60390",
+    "url": "/Clicky-Game/static/js/main.981ed03b.chunk.js"
+  },
+  {
+    "revision": "17e173d90699804f14d84d05f3ad08e6",
+    "url": "/Clicky-Game/static/media/mario.17e173d9.png"
+  },
+  {
+    "revision": "735d14fe1475da4e915396cc9f8cbebc",
+    "url": "/Clicky-Game/static/media/marth.735d14fe.png"
+  },
+  {
+    "revision": "d23cf3f4581949dd46b795c5d0f723fe",
+    "url": "/Clicky-Game/static/media/megaman.d23cf3f4.png"
+  },
+  {
+    "revision": "62279f2fef8faab07fd20bcd26572a22",
+    "url": "/Clicky-Game/static/media/ness.62279f2f.png"
+  },
+  {
+    "revision": "bf80b0fa2db09938a52dffa0ac9bbc8a",
+    "url": "/Clicky-Game/static/media/pac-man.bf80b0fa.png"
+  },
+  {
+    "revision": "961366012720722c723e34ff800df817",
+    "url": "/Clicky-Game/static/media/pikachu.96136601.png"
+  },
+  {
+    "revision": "121d710be40485aa4458aef5037147de",
+    "url": "/Clicky-Game/static/media/rob.121d710b.png"
+  },
+  {
+    "revision": "482cd4ef344eb0c9f73211798d44a94f",
+    "url": "/Clicky-Game/static/media/sonic.482cd4ef.png"
+  },
+  {
+    "revision": "0fca74bf5d0c755107e5fc361bfe0fa0",
+    "url": "/Clicky-Game/static/media/villager.0fca74bf.png"
+  },
+  {
+    "revision": "84d525ab9622d0cdd4210de6efacf5a6",
+    "url": "/Clicky-Game/index.html"
+  }
+];
